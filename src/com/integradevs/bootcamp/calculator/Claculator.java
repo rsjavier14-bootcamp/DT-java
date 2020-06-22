ce55761d958becf01b0d628a35ed8ef7a663fa8f
Javier Rios Selent <rsjavier14@gmail.com>
2020-06-22 10:36:43 -0400

    added a calculator class with the add method. Closes #1

---
+++ src/com/integradevs/bootcamp/calculator/Claculator.java
@@ -0,0 +1,23 @@
+package com.integradevs.bootcamp.calculator;
+
+/**
+ * Calculator class
+ * 
+ * This class has a method to add two numbers
+ * 
+ * @author javier14-bootcamp
+ *
+ */
+public class Claculator {
+
+	/**
+	 * The addition method receives two integers and performs a sum between them.
+	 * 
+	 * @param a First number to add
+	 * @param b Second number to add
+	 * @return the sum of both numbers of type int
+	 */
+	public int add(int a, int b) {
+		return a + b;
+	}
+}
